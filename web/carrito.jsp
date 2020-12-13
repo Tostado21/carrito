6d8689b3bed97b1ca8e2380e269430ee8bd92442
tostado <tostado@DESKTOP-CKQEIJU>
2020-12-12 22:10:43 -0600

    fijate en funcion.js si funciona

--- web/carrito.jsp
+++ web/carrito.jsp
@@ -140,8 +140,7 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>    
         
         <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
-        <script src = "https://unpkg.com/sweetalert/dist/sweetalert.min.js "></script> 
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
         <script src="js/funciones.js" type="text/javascript"></script>
     </body>
 </html>
--- web/js/funciones.js
+++ web/js/funciones.js
@@ -2,14 +2,31 @@
 $(document).ready(function() {
     $("tr #btnDelete").click(function() {
         var idp = $(this).parent().find("#idp").val();
-            
-            eliminar(idp);
+        swal({
+        title: "Are you sure?",
+                text: "Once deleted, you will not be able to recover this imaginary file!",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+        }).then((willDelete) = > {
+                if (willDelete) {
+                    eliminar(idp);
+                    swal("Poof! Your imaginary file has been deleted!", {
+                        icon: "success",
+                    }).then((willDelete)=>{
+                        if (willDelete) {
+                            parent.location.href="Controlador?accion=Carrito";
+                        }
+                    });
+                } else {
+                    swal("Your imaginary file is safe!");
+                }
+            });
     });
-
     function eliminar(idp) {
         var url = "Controlador?accion=Delete";
         $.ajax({
-            type: 'POST',
+        type: 'POST',
             url: url,
             data: "idp=" + idp,
             success: function(data, textStatus, jqXHR) {
